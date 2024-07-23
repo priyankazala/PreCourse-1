aa93bc163ae393e815afadcf1c8c0af297394603
priyankazala <pzala@stevens.edu>
2024-07-23 18:33:39 -0400

    Precourse1 done.

--- Exercise_2.py
+++ Exercise_2.py
@@ -6,10 +6,24 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.head = None
         
     def push(self, data):
-        
+        if self.head is None:
+            self.head = Node(data)
+        else:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head = new_node
+            return self.head
     def pop(self):
+        if self.head is None:
+            return None
+        else:
+            temp = self.head
+            self.head = self.head.next
+            temp.next = None
+            return temp.data
         
 a_stack = Stack()
 while True:
--- Exercise_3.py
+++ Exercise_3.py
@@ -3,6 +3,9 @@ class ListNode:
     A node in a singly-linked list.
     """
     def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
     
 class SinglyLinkedList:
     def __init__(self):
@@ -12,11 +15,22 @@ class SinglyLinkedList:
         """
         self.head = None
 
+
     def append(self, data):
         """
         Insert a new element at the end of the list.
         Takes O(n) time.
         """
+        if self.head is None:
+            self.head = ListNode(data)
+        else:
+            new_node = ListNode(data)
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = new_node
+            
+
         
     def find(self, key):
         """
@@ -24,9 +38,32 @@ class SinglyLinkedList:
         `key`. Return the element or `None` if not found.
         Takes O(n) time.
         """
+        current = self.head
+        while current is not None:
+            if current.data == key:
+                return current
+            current = current.next
+        return ("Key not in Linked List")    
         
     def remove(self, key):
         """
         Remove the first occurrence of `key` in the list.
         Takes O(n) time.
         """
+        current = self.head
+        previous = None
+        found = False
+        while current is not None and not found:
+            if current.data == key and current is self.head:
+                self.head = current.next
+                found = True
+                break
+            elif current.data == key:
+                previous.next = current.next
+                found = True
+                break
+            else:
+                previous = current
+                current = current.next
+        if not found:
+            return ("Key not in Linked List")
